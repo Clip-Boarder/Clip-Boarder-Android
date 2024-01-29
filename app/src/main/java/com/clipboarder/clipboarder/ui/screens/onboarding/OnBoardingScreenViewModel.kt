a2de3f158b75d6a432c03ba293421ccfe74d1d62
Sohn Young Jin <sonkim1001@naver.com>
2024-01-29 23:24:24 +0900

    feat: 각 화면별 ViewModel 기본 코드 추가 및 kDoc 작성

--- app/src/main/java/com/clipboarder/clipboarder/ui/screens/onboarding/OnBoardingScreenViewModel.kt
+++ app/src/main/java/com/clipboarder/clipboarder/ui/screens/onboarding/OnBoardingScreenViewModel.kt
@@ -1,4 +1,14 @@
 package com.clipboarder.clipboarder.ui.screens.onboarding
 
-class OnBoardingScreenViewModel {
+import androidx.lifecycle.ViewModel
+
+/**
+ * Onboarding screen view model.
+ *
+ * View model for onboarding screen.
+ *
+ * @since 1.0.0
+ * @author YoungJin Sohn
+ */
+class OnBoardingScreenViewModel : ViewModel() {
 }
---
+++ app/src/main/java/com/clipboarder/clipboarder/ui/screens/storage/StorageViewModel.kt
@@ -0,0 +1,15 @@
+package com.clipboarder.clipboarder.ui.screens.storage
+
+import androidx.lifecycle.ViewModel
+
+
+/**
+ * Storage screen view model.
+ *
+ * View model for storage screen.
+ *
+ * @since 1.0.0
+ * @author YoungJin Sohn
+ */
+class StorageViewModel : ViewModel(){
+}
