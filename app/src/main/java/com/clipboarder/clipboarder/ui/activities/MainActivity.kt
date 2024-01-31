d3d7cbccb6946a54cc87d77fa73d18709a81fb04
Sohn Young Jin <sonkim1001@naver.com>
2024-01-31 20:09:22 +0900

    feat: 앱에 기본 폰트 적용

--- app/src/main/java/com/clipboarder/clipboarder/ui/activities/MainActivity.kt
+++ app/src/main/java/com/clipboarder/clipboarder/ui/activities/MainActivity.kt
@@ -3,8 +3,15 @@ package com.clipboarder.clipboarder.ui.activities
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Typography
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.sp
 import androidx.navigation.compose.rememberNavController
+import com.clipboarder.clipboarder.R
 import com.clipboarder.clipboarder.ui.navigation.AppNavigation
 
 /**
@@ -39,5 +46,27 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun AppScreen() {
     val navController = rememberNavController()
-    AppNavigation(navController)
+    val fontFamily = FontFamily(
+        Font(R.font.pretendard_thin, FontWeight.Thin),
+        Font(R.font.pretendard_extralight, FontWeight.ExtraLight),
+        Font(R.font.pretendard_light, FontWeight.Light),
+        Font(R.font.pretendard_regular),
+        Font(R.font.pretendard_medium, FontWeight.Medium),
+        Font(R.font.pretendard_semibold, FontWeight.SemiBold),
+        Font(R.font.pretendard_bold, FontWeight.Bold),
+        Font(R.font.pretendard_extrabold, FontWeight.ExtraBold),
+        Font(R.font.pretendard_black, FontWeight.Black),
+    )
+
+    MaterialTheme(
+        typography = Typography(
+            bodyMedium = androidx.compose.ui.text.TextStyle(
+                fontFamily = fontFamily,
+                fontWeight = FontWeight.Medium,
+                fontSize = 16.sp
+            )
+        )
+    ) {
+        AppNavigation(navController)
+    }
 }
