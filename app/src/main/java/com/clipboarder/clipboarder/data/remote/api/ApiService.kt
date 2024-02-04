4a6e1b9fb70dc63b06ca2a0b2713a9371e5d51dc
Sohn Young Jin <sonkim1001@naver.com>
2024-02-04 23:37:33 +0900

    feat: AppModule 내 필요한 의존성 관련 코드 추가

--- app/src/main/java/com/clipboarder/clipboarder/data/remote/api/ApiService.kt
+++ app/src/main/java/com/clipboarder/clipboarder/data/remote/api/ApiService.kt
@@ -1,2 +1,16 @@
 package com.clipboarder.clipboarder.data.remote.api
 
+import retrofit2.http.GET
+import retrofit2.http.Headers
+import retrofit2.http.POST
+
+/**
+ * ApiService
+ *
+ * This interface provides the necessary API endpoints for the application.
+ *
+ * @since 1.0.0
+ * @author YoungJin Sohn
+ */
+interface ApiService {
+}
