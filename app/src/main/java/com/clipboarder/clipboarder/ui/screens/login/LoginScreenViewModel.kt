4c60100caa473f289c174911b4d601b44ef9e879
Sohn Young Jin <sonkim1001@naver.com>
2024-02-18 18:22:18 +0900

    feat: 로그인 프로세스 로직 추가

--- app/src/main/java/com/clipboarder/clipboarder/ui/screens/login/LoginScreenViewModel.kt
+++ app/src/main/java/com/clipboarder/clipboarder/ui/screens/login/LoginScreenViewModel.kt
@@ -1,6 +1,15 @@
 package com.clipboarder.clipboarder.ui.screens.login
 
+import android.util.Log
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.clipboarder.clipboarder.data.repository.UserRepository
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.launch
+import javax.inject.Inject
 
 
 /**
@@ -11,5 +20,49 @@ import androidx.lifecycle.ViewModel
  * @since 1.0.0
  * @author YoungJin Sohn
  */
-class LoginScreenViewModel : ViewModel(){
+@HiltViewModel
+class LoginScreenViewModel @Inject constructor(private val userRepository: UserRepository) :
+    ViewModel() {
+    private val _isLoginProcessing = MutableStateFlow(false)
+    val isLoginProcessing = _isLoginProcessing.asStateFlow()
+
+    private val _isLoginSuccess = MutableStateFlow<Boolean?>(null)
+    val isLoginSuccess = _isLoginSuccess.asStateFlow()
+
+    fun startLoginProcess() {
+        _isLoginProcessing.value = true
+    }
+
+    fun endLoginProcess() {
+        _isLoginProcessing.value = false
+    }
+
+    fun signInWithGoogleAccount(account: GoogleSignInAccount) {
+        _isLoginSuccess.value = null
+        viewModelScope.launch {
+            try {
+                userRepository.signUp(
+                    account.email!!,
+                    "testpw",
+                    account.displayName!!,
+                    account.photoUrl.toString(),
+                    "google"
+                ).collect { responseDto ->
+                    if (responseDto.result!! == true) {
+                        // userRepository.saveToken(responseDto.data?.accessToken)
+                        Log.d("LoginScreenViewModel", "Token: ${responseDto.data?.accessToken}")
+                        _isLoginSuccess.value = true
+                    } else {
+                        Log.d("LoginScreenViewModel", "Error: error while signing up")
+                        _isLoginSuccess.value = false
+                    }
+                }
+            } catch (e: Exception) {
+                _isLoginSuccess.value = false
+                Log.d("LoginScreenViewModel", "Error in googleSignIn: $e")
+            } finally {
+                endLoginProcess()
+            }
+        }
+    }
 }
