7948acf839221a3ecdb720ea711e7cfefecd1ae4
Sohn Young Jin <sonkim1001@naver.com>
2024-02-26 23:47:01 +0900

    feat: accessToken 확인 및 텍스트 업로드 로직 구현

--- app/src/main/java/com/clipboarder/clipboarder/ui/activities/ContextMenuActivity.kt
+++ app/src/main/java/com/clipboarder/clipboarder/ui/activities/ContextMenuActivity.kt
@@ -2,15 +2,42 @@ package com.clipboarder.clipboarder.ui.activities
 
 import android.content.Intent
 import android.net.Uri
+import android.os.Build
+import android.os.Build.VERSION
 import android.os.Bundle
 import android.widget.Toast
 import androidx.activity.ComponentActivity
+import androidx.lifecycle.lifecycleScope
+import com.clipboarder.clipboarder.data.repository.ContentRepository
+import com.clipboarder.clipboarder.data.repository.UserRepository
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.flow.catch
+import kotlinx.coroutines.launch
+import javax.inject.Inject
 
+/**
+ * ContextMenuActivity
+ *
+ * This activity is used to handle the context menu actions.
+ *
+ * @author YoungJin Sohn
+ * @since v1.0.0
+ */
 @AndroidEntryPoint
 class ContextMenuActivity : ComponentActivity() {
+    @Inject
+    lateinit var userRepository: UserRepository
+
+    @Inject
+    lateinit var contentRepository: ContentRepository
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        if (userRepository.getAccessToken() == null) {
+            Toast.makeText(this, "로그인이 필요합니다", Toast.LENGTH_SHORT).show()
+            return
+        }
 
         val action = intent.action
         intent.type?.let { type ->
@@ -48,8 +75,11 @@ class ContextMenuActivity : ComponentActivity() {
     }
 
     private fun uploadImages(intent: Intent) {
-        // 모든 이미지의 경로를 하나의 텍스트로 합쳐서 Toast로 보내기
-        val imageUriList = intent.getParcelableArrayListExtra<Uri>(Intent.EXTRA_STREAM)
+        val imageUriList = if (VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+            intent.getParcelableArrayListExtra(Intent.EXTRA_STREAM, Uri::class.java)
+        } else {
+            intent.getParcelableArrayListExtra(Intent.EXTRA_STREAM)
+        }
         imageUriList?.let {
             val combinedText = imageUriList.joinToString(separator = "\n") { uri ->
                 uri.path.toString()
@@ -61,7 +91,26 @@ class ContextMenuActivity : ComponentActivity() {
     private fun uploadText(intent: Intent) {
         val selectedText = intent.getCharSequenceExtra(Intent.EXTRA_PROCESS_TEXT)
         if (selectedText != null) {
-            Toast.makeText(this, "선택한 텍스트: $selectedText", Toast.LENGTH_SHORT).show()
+            lifecycleScope.launch {
+                contentRepository.copyTextToClipboarder(selectedText.toString())
+                    .catch { e ->
+                        // Handle any errors, e.g., show a toast message
+                        Toast.makeText(
+                            this@ContextMenuActivity,
+                            "Error: ${e.message}",
+                            Toast.LENGTH_LONG
+                        ).show()
+                    }
+                    .collect { response ->
+                        Toast.makeText(
+                            this@ContextMenuActivity,
+                            "Text uploaded successfully",
+                            Toast.LENGTH_SHORT
+                        ).show()
+                    }
+            }
+        } else {
+            Toast.makeText(this, "선택한 텍스트가 없습니다", Toast.LENGTH_SHORT).show()
         }
     }
 
