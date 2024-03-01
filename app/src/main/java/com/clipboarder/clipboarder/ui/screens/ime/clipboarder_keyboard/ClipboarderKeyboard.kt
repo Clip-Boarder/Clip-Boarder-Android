05856a4a24dc9c585e522bb33609731a06993b09
Sohn Young Jin <sonkim1001@naver.com>
2024-03-01 22:39:13 +0900

    feat: 클립보더 키보드 표시 시 API로부터 콘텐츠 데이터를 불러와 표시하고, 아이템 터치 시 데이터를 붙여넣도록 구현

--- app/src/main/java/com/clipboarder/clipboarder/ui/screens/ime/clipboarder_keyboard/ClipboarderKeyboard.kt
+++ app/src/main/java/com/clipboarder/clipboarder/ui/screens/ime/clipboarder_keyboard/ClipboarderKeyboard.kt
@@ -1,19 +1,43 @@
 package com.clipboarder.clipboarder.ui.screens.ime.clipboarder_keyboard
 
 import android.inputmethodservice.InputMethodService
-import android.util.Log
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.Button
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
+import coil.compose.rememberAsyncImagePainter
+import com.clipboarder.clipboarder.BuildConfig
+import com.clipboarder.clipboarder.data.remote.dto.ContentDto
 import com.clipboarder.clipboarder.data.repository.ContentRepository
 import com.clipboarder.clipboarder.data.repository.UserRepository
 
+/**
+ * Clipboarder keyboard.
+ *
+ * Clipboarder keyboard for clipboarder.
+ *
+ * @param inputMethodService input method service
+ * @param userRepository user repository
+ * @param contentRepository content repository
+ */
 @Composable
 fun ClipboarderKeyboard(
     inputMethodService: InputMethodService,
@@ -22,17 +46,74 @@ fun ClipboarderKeyboard(
     viewModel: ClipboarderKeyboardViewModel = hiltViewModel()
 ) {
     viewModel.setRepositories(userRepository, contentRepository)
+    viewModel.loadContentList()
 
-    Column(
-        modifier = Modifier.fillMaxSize(),
-        verticalArrangement = Arrangement.Center,
-        horizontalAlignment = Alignment.CenterHorizontally
+    val contentList by viewModel.contentList.collectAsState()
+
+    LazyVerticalGrid(
+        columns = GridCells.Fixed(3),
+        contentPadding = PaddingValues(4.dp),
+        modifier = Modifier.fillMaxSize()
     ) {
-        Text(text = "클립보더 키보드 영역")
-        Button(onClick = {
+        items(contentList.size) { index ->
+            ClipboarderContentItem(inputMethodService = inputMethodService, contentItem = contentList[index])
+        }
+    }
+}
 
-        }) {
-            Text(text = "토큰 가져오기")
+/**
+ * Clipboarder content item.
+ *
+ * Clipboarder content item for clipboarder keyboard.
+ *
+ * @param inputMethodService input method service
+ * @param contentItem content item
+ */
+@Composable
+fun ClipboarderContentItem(inputMethodService: InputMethodService, contentItem: ContentDto.ContentObjectDto) {
+    Box(
+        modifier = Modifier
+            .padding(4.dp)
+            .aspectRatio(1f)
+            .clip(RoundedCornerShape(4.dp))
+            .background(Color.LightGray)
+            .clickable {
+                when (contentItem.contentType) {
+                    "text" -> {
+                        inputMethodService.currentInputConnection?.commitText(
+                            contentItem.content,
+                            1
+                        )
+                    }
+
+                    "image" -> {
+                        inputMethodService.currentInputConnection?.commitText(
+                            BuildConfig.clipboarderBaseUrl + contentItem.content,
+                            1
+                        )
+                    }
+
+                    else -> return@clickable
+                }
+
+            }
+    ) {
+        when (contentItem.contentType) {
+            "text" -> Text(
+                text = contentItem.content ?: "",
+                modifier = Modifier.padding(8.dp),
+                maxLines = 3,
+                overflow = TextOverflow.Ellipsis
+            )
+
+            "image" -> Image(
+                painter = rememberAsyncImagePainter(BuildConfig.clipboarderBaseUrl + contentItem.content),
+                contentDescription = null,
+                modifier = Modifier.fillMaxSize(),
+                contentScale = ContentScale.Crop
+            )
+
+            else -> Unit
         }
     }
 }
--- app/src/main/java/com/clipboarder/clipboarder/ui/screens/ime/clipboarder_keyboard/ClipboarderKeyboardViewModel.kt
+++ app/src/main/java/com/clipboarder/clipboarder/ui/screens/ime/clipboarder_keyboard/ClipboarderKeyboardViewModel.kt
@@ -1,20 +1,67 @@
 package com.clipboarder.clipboarder.ui.screens.ime.clipboarder_keyboard
 
+import android.util.Log
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.clipboarder.clipboarder.data.remote.dto.ContentDto
 import com.clipboarder.clipboarder.data.repository.ContentRepository
 import com.clipboarder.clipboarder.data.repository.UserRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
+/**
+ * Clipboarder keyboard view model.
+ *
+ * View model for clipboarder keyboard.
+ *
+ * @since 1.0.0
+ * @author YoungJin Sohn
+ */
 @HiltViewModel
 class ClipboarderKeyboardViewModel @Inject constructor(
 ) : ViewModel() {
     private lateinit var userRepository: UserRepository
     private lateinit var contentRepository: ContentRepository
 
+    private val _contentList = MutableStateFlow<List<ContentDto.ContentObjectDto>>(emptyList())
+    val contentList: StateFlow<List<ContentDto.ContentObjectDto>> = _contentList
+
+    /**
+     * Set repositories.
+     *
+     * Set user and content repositories.
+     *
+     * @param userRepository user repository
+     * @param contentRepository content repository
+     */
     fun setRepositories(userRepository: UserRepository, contentRepository: ContentRepository) {
         this.userRepository = userRepository
         this.contentRepository = contentRepository
+    }
+
+    /**
+     * Load content list.
+     *
+     * Load content list from clipboarder.
+     */
+    fun loadContentList() {
+        viewModelScope.launch {
+            try {
+                contentRepository.getContentListFromClipboarder().collect { responseDto ->
+                    if (responseDto.result!!) {
+                        _contentList.value = responseDto.data?.contentList!!
+                    } else {
+                        throw Exception("Error: error while getting content list")
+                    }
+                }
+            } catch (e: Exception) {
+                _contentList.value = emptyList()
+                Log.e("ClipboarderKeyboardViewModel", "Error: error while getting content list", e)
+            }
+        }
     }
 }
 
