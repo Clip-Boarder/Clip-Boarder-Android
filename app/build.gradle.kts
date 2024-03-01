9cd559f5333c7a6fef8fc182934ed4bd65519ddc
Sohn Young Jin <sonkim1001@naver.com>
2024-03-01 15:50:09 +0900

    chore: compose 라이브러리 업데이트 및 foundation 추가

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -86,11 +86,12 @@ dependencies {
     implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.7.0")
     implementation("androidx.activity:activity-compose:1.8.2")
-    implementation(platform("androidx.compose:compose-bom:2023.03.00"))
+    implementation(platform("androidx.compose:compose-bom:2024.02.01"))
     implementation("androidx.compose.ui:ui")
     implementation("androidx.compose.ui:ui-graphics")
     implementation("androidx.compose.ui:ui-tooling-preview")
     implementation("androidx.compose.material3:material3")
+    implementation("androidx.compose.foundation:foundation")
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.11.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
@@ -101,8 +102,8 @@ dependencies {
     implementation("androidx.compose.runtime:runtime-livedata:1.6.0")
     implementation("com.squareup.retrofit2:retrofit:2.9.0")             // Retrofit
     implementation("com.squareup.retrofit2:converter-gson:2.9.0")       // Retrofit Converter
-    implementation("com.google.dagger:hilt-android:2.50")
-    implementation("androidx.lifecycle:lifecycle-service:2.7.0")               // Dagger-Hilt
+    implementation("com.google.dagger:hilt-android:2.50")               // Dagger-Hilt
+    implementation("androidx.lifecycle:lifecycle-service:2.7.0")        // Lifecycle Service
     ksp("com.google.dagger:hilt-android-compiler:2.50")                 // Hilt Compiler
     implementation("androidx.hilt:hilt-navigation-compose:1.1.0")       // Hilt Navigation
     implementation("com.google.android.gms:play-services-auth:21.0.0")  // Google Sign-In
@@ -112,7 +113,7 @@ dependencies {
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
-    androidTestImplementation(platform("androidx.compose:compose-bom:2023.03.00"))
+    androidTestImplementation(platform("androidx.compose:compose-bom:2024.02.01"))
     androidTestImplementation("androidx.compose.ui:ui-test-junit4")
     debugImplementation("androidx.compose.ui:ui-tooling")
     debugImplementation("androidx.compose.ui:ui-test-manifest")
