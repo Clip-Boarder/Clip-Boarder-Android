943d7c431dce24af3e9219fe87f562d806cddb71
Sohn Young Jin <sonkim1001@naver.com>
2024-03-27 02:55:51 +0900

    feat: 불필요한 ViewModel 제거

--- app/src/main/java/com/clipboarder/clipboarder/service/ClipboarderKeyboardServiceViewModel.kt
+++
@@ -1,9 +0,0 @@
-package com.clipboarder.clipboarder.service
-
-import androidx.lifecycle.ViewModel
-import dagger.hilt.android.lifecycle.HiltViewModel
-import javax.inject.Inject
-
-@HiltViewModel
-class ClipboarderKeyboardServiceViewModel @Inject constructor() : ViewModel() {
-}
